6815ecd17b1129ef181b2105284fa06a559cd821
han6yu1 <s2234@e-mirim.hs.kr.com>
2023-03-23 19:50:43 +0900

    Event Programming

--- app/src/main/java/kr/hs/emirim/codelayout/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/codelayout/MainActivity.kt
@@ -2,10 +2,19 @@ package kr.hs.emirim.codelayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var btn1 : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn1 = findViewById(R.id.btn1)
+
+        btn1.setOnClickListener{
+            Toast.makeText(applicationContext, "짜잔", Toast.LENGTH_SHORT).show()
+        }
     }
 }
